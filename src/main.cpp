f0271ac5dadf536b2bc823450f1645c6cf49d34b
MatildosKiller <lsnkandrey@outlook.com>
2025-03-03 21:46:05 +0500

    check box

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <iostream>
-
+// Copyright 2020 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
 #include <memory>  // for shared_ptr, __shared_ptr_access
 #include <string>  // for operator+, to_string
 
@@ -10,10 +9,16 @@
 #include "ftxui/component/component_base.hpp"      // for ComponentBase
 #include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
 #include "ftxui/dom/elements.hpp"  // for separator, gauge, text, Element, operator|, vbox, border
+#include <iostream>
 
 using namespace ftxui;
 
-
+// This is a helper function to create a button with a custom style.
+// The style is defined by a lambda function that takes an EntryState and
+// returns an Element.
+// We are using `center` to center the text inside the button, then `border` to
+// add a border around the button, and finally `flex` to make the button fill
+// the available space.
 ButtonOption Style() {
     auto option = ButtonOption::Animated();
     option.transform = [](const EntryState& s) {
@@ -25,28 +30,38 @@ ButtonOption Style() {
         };
     return option;
 }
-int main(void) {
-    using namespace ftxui;
 
+int main() {
+    int value = 50;
 
-    
+    // clang-format off
+    auto btn_dec_01 = Button("-1", [&] { value -= 1; }, Style());
+    auto btn_inc_01 = Button("+1", [&] { value += 1; }, Style());
+    auto btn_dec_10 = Button("-10", [&] { value -= 10; }, Style());
+    auto btn_inc_10 = Button("+10", [&] { value += 10; }, Style());
+    // clang-format on
 
-    
-    // Define the document
-    Element document =
-        hbox({
-          text("лево") | border,
-          text("middle") | border | flex,
-          text("right") | border,
-            });
+    // The tree of components. This defines how to navigate using the keyboard.
+    // The selected `row` is shared to get a grid layout.
+    int row = 0;
+    auto buttons = Container::Vertical({
+        Container::Horizontal({btn_dec_01, btn_inc_01}, &row) | flex,
+        Container::Horizontal({btn_dec_10, btn_inc_10}, &row) | flex,
+        });
 
-    auto screen = Screen::Create(
-        Dimension::Full(),       // Width
-        Dimension::Fit(document) // Height
-    );
-    Render(screen, document);
-    screen.Print();
-    char key{};
-    getchar();
-    return EXIT_SUCCESS;
+    // Modify the way to render them on screen:
+    auto component = Renderer(buttons, [&] {
+        return vbox({
+                   text("value = " + std::to_string(value)),
+                   separator(),
+                   buttons->Render() | flex,
+            }) |
+            flex | border;
+        });
+
+    auto screen = ScreenInteractive::FitComponent();
+    screen.Loop(component);
+    int f{};
+    std::cin >> f;
+    return 0;
 }
